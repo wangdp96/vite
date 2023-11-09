3930179bfce9c833264a42a60a1706ebe97bdf45
wdp01015138 <wdp01015138@alibaba-inc.com>
2023-11-09 11:28:57 +0800

    feat：生产环境打包优化

--- vite-vue/vite.config.ts
+++ vite-vue/vite.config.ts
@@ -2,17 +2,20 @@
  * @Author: wdp01015138 wdp01015138@alibaba-inc.com
  * @Date: 2023-10-24 16:06:33
  * @LastEditors: wdp01015138 wdp01015138@alibaba-inc.com
- * @LastEditTime: 2023-10-26 11:35:29
+ * @LastEditTime: 2023-11-08 19:15:10
  * @FilePath: /vite/vite01/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import legacy from "@vitejs/plugin-legacy";
 
+import path from "path";
+// const { resolve } = require("path");
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), legacy({ targets: ["defaults", "not IE 11"] })],
   optimizeDeps: {
     // include: ["esm-dep"],
   },
@@ -25,6 +28,15 @@ export default defineConfig({
   css: {
     modules: {
       localsConvention: "camelCase",
+    },
+  },
+  build: {
+    target: "es2015",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        nested: path.resolve(__dirname, "nested/index.html"),
+      },
     },
   },
 });
--- vite-vue2/vite.config.js
+++ vite-vue2/vite.config.js
@@ -2,7 +2,7 @@
  * @Author: wdp01015138 wdp01015138@alibaba-inc.com
  * @Date: 2023-10-25 17:11:14
  * @LastEditors: wdp01015138 wdp01015138@alibaba-inc.com
- * @LastEditTime: 2023-11-02 17:04:16
+ * @LastEditTime: 2023-11-08 17:20:58
  * @FilePath: /vite/vite-vue2/vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,4 +15,7 @@ export default defineConfig({
     // include: ['MyComponent'],
   },
   presets: ['@babel/preset-env'],
+  build: {
+    target: 'es2015',
+  },
 })
